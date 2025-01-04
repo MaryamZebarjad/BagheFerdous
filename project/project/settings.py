b53bb21ac729fe19935016aa0eb2a3f4ff516c8d
Ahmad-Mirzaei <ahmadmirzaei.py@gmail.com>
2025-01-04 18:53:40 +0330

    Update settings.py
    Change in settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,7 +67,8 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates/'),],
+        # 'DIRS': [os.path.join(BASE_DIR,'templates'),],
+        'DIRS': [BASE_DIR /'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,11 +134,17 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static/'),)
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS=(os.path.join(BASE_DIR,'static/'),)
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/medias/'
 
 
 # Default primary key field type
